fc67cf6b125e8b3e05c121f256242ce4249fee0f
Dimitri Rosenberg <dimitri@range-app.com>
2016-01-20 21:12:20 +0300

    feat(css): style the thankyou page

--- app/server/views/thankyou.tsx
+++ app/server/views/thankyou.tsx
@@ -5,10 +5,18 @@ import Page from './layouts/page';
 export default class Thankyou extends React.Component<{}, {}> {
   render() {
     return (
-      <Page>
-        <h1>Thank You!</h1>
-        <p>We have received your submission.</p>
-        <a href="javascript:history.back()">Go Back</a>
+      <Page bodyClass="thankyou">
+        <main>
+          <section className="thankyou">
+            <h1>Thank you!</h1>
+            <h2>We have received your submission.</h2>
+            <a className="button" href="javascript:history.back()">Go Back</a>
+          </section>
+
+          <footer className="copyright">
+            <p>Powered by <a href="https://figureapp.com" target="_blank">Figure</a></p>
+          </footer>
+        </main>
       </Page>
     );
   }
