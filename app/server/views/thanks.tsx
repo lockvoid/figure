219ebd9efe0545df8a1076ea2b1b74fe8e9159df
Dimitri Rosenberg <dimitri@range-app.com>
2016-04-16 16:18:13 +0300

    refactor(css): simplify the thanks page

--- app/server/views/thanks.tsx
+++ app/server/views/thanks.tsx
@@ -2,22 +2,17 @@ import * as React from 'react';
 
 import { Page } from './layouts/page';
 
-export default class Thanks extends React.Component<{}, {}> {
-  render() {
-    return (
-      <Page className="thankyou">
-        <main>
-          <section className="thankyou">
-            <h1>Thank you!</h1>
-            <h2>We have received your submission.</h2>
-            <a className="button" href="javascript:history.back()">Go Back</a>
-          </section>
+export default () => (
+  <Page className="thanks">
+    <main>
+        <h1>Thank you!</h1>
+        <h2>We have received your submission.</h2>
 
-          <footer className="copyright">
-            <p>Powered by <a href={process.env.FIGURE_URL} target="_blank">Figure</a></p>
-          </footer>
-        </main>
-      </Page>
-    );
-  }
-}
+        <a className="button raised primary" href="javascript:history.back()">Go Back</a>
+
+        <footer>
+          Powered by <a href={process.env.FIGURE_URL} target="_blank">Figure</a>.
+        </footer>
+    </main>
+  </Page>
+);
